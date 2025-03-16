d12f8c671607c520f8af50e00317e777d5eeeef0
Carlostrdom <carlos.a.t.m93@gmail.com>
2025-03-16 18:50:22 -0500

    pruebatecnica

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -1,7 +1,7 @@
 export default () => ({
   database: {
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432', 10), // Valor predeterminado como cadena
+    port: parseInt(process.env.DB_PORT || '5432', 10), 
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     name: process.env.DB_NAME || 'movies_db',
